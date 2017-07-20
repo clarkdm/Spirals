cfef780eed3fa9fcda1651fe42b57035d2703bd1
clarkdm <clarkdm@users.noreply.github.com>
2017-07-20 22:12:30 +0100

    we made it!!!!!!!

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Spirals
+Spirals
+
+Description
+The user enters a number. Make a spiral that begins with 1 and starts from the top left, going towards the right, and ends with the square of that number.
+
+Input description
+Let the user enter a number.
+
+Output description
+Note the proper spacing in the below example. You'll need to know the number of digits in the biggest number.
+
+Input
+
+5
+
+Output
+
+1  2  3  4  5
+16 17 18 19 6
+15 24 25 20 7
+14 23 22 21 8
+13 12 11 10 9
---
+++ src/DD/spiral.java
@@ -0,0 +1,85 @@
+package DD;
+
+public class spiral {
+
+public int[][] spiralNum;
+public int target;
+int target2;
+
+public spiral(int target) {
+this.target = target;
+target2 = 0;
+target2 = target + target2;
+spiralNum = new int[target][target];
+}
+
+public void calculate() {
+int counter = 1;
+
+int xMax = target - 1;
+int yMax = target - 1;
+int xMin = 0;
+int yMin = 0;
+boolean direction = true;
+
+for (int x = xMin; x <= xMax; x++) {
+spiralNum[x][0] = counter;
+counter++;
+}
+yMin++;
+while (target > 1) {
+if (direction) {
+
+for (int y = yMin; y < yMax; y++) {
+spiralNum[xMax][y] = counter;
+counter++;
+}
+for (int x = xMax; x >= xMin; x--) {
+spiralNum[x][yMax] = counter;
+counter++;
+}
+xMax = xMax - 1;
+yMax = yMax - 1;
+} else {
+
+for (int y = yMax; y > yMin; y--) {
+spiralNum[xMin][y] = counter;
+counter++;
+}
+for (int x = xMin; x <= xMax; x++) {
+spiralNum[x][yMin] = counter;
+counter++;
+}
+
+xMin = xMin - 1;
+yMin = yMin - 1;
+}
+target--;
+direction = !direction;
+}
+return;
+}
+
+public String toString() {
+
+String result = "";
+for (int y = 0; y < target2; y++) {
+for (int x = 0; x < target2; x++) {
+result = result + spiralNum[x][y] + '\t';
+
+}
+result = result +'\n';
+}
+return result;
+}
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+spiral s = new spiral(3);
+s.calculate();
+System.out.println(s.toString());
+
+}
+
+}
